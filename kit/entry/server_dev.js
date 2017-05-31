fc34184bfda146b5970e145abc315b6d6f40f5ec
zanjs <root@zanjs.com>
2017-05-31 14:26:44 +0800

    :cat: init

---
+++ kit/entry/server_dev.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-console */
+
+// Production server entry point.  Spawns the server on default HOST:PORT
+
+// ----------------------
+// IMPORTS
+
+/* NPM */
+
+// Chalk terminal library
+import chalk from 'chalk';
+
+/* Local */
+
+// Import console messages
+import { logServerStarted } from 'kit/lib/console';
+
+// Extend the server base
+import server, { createReactHandler, staticMiddleware } from './server';
+
+// ----------------------
+
+// Host and port -- from the environment
+const HOST = process.env.SERVER_DEV_HOST || 'localhost';
+const PORT = process.env.SERVER_DEV_PORT || 8081;
+
+// Get manifest values
+const css = '/assets/css/style.css';
+const scripts = [
+  'vendor.js',
+  'browser.js'].map(key => `/${key}`);
+
+// Spawn the server
+server.then(({ router, app }) => {
+  // Create proxy to tunnel requests to the browser `webpack-dev-server`
+  router.get('/*', createReactHandler(css, scripts));
+
+  // Connect the development routes to the server
+  app
+    .use(staticMiddleware())
+    .use(router.routes())
+    .use(router.allowedMethods());
+
+  app.listen({ host: HOST, port: PORT }, () => {
+    logServerStarted({
+      type: 'server-side rendering',
+      host: HOST,
+      port: PORT,
+      chalk: chalk.bgYellow.black,
+    });
+  });
+});
